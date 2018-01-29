df3176da92039d0782c464758c6fb1dfe2208a98
lansenchan <274248453@qq.com>
2018-01-29 23:42:02 +0800

    finish catalog gui

--- editor/test.py
+++ editor/test.py
@@ -1,43 +1,46 @@
 # -*- coding=utf-8 -*-
-import re
+#import re
 
 
 import edit
 
 import Tkinter as tk
 
-def test_gui():
 
-    root = tk.Tk()
+root = tk.Tk()
 
-    f = tk.Frame(root)
-
-
-    t = tk.Text(f)
-
-    t.insert("1.0", edit.get_content())
-
-    v = tk.StringVar()
-    c = tk.Entry(f, textvariable = v)
-    c.focus_set()
-
-    def do_command(e):
-        eval(v.get())
-        v.set("")
-
-    c.bind("<Return>", do_command)
-
-    t.pack()
-    c.pack()
+f = tk.Frame(root)
 
 
+t = tk.Text(f)
 
-    f.pack()
+t.insert("1.0", edit.Content("./code_demo.py").get_content())
 
-    root.mainloop()
+v = tk.StringVar()
+c = tk.Entry(f, textvariable = v)
+c.focus_set()
 
-t = '''def handle_special(self, special_key):'''
-p = re.compile("[a-zA-Z_]+")
-d = p.search(t)
-print d
+def do_command(e):
+    print eval(v.get())
+    v.set("")
+
+c.bind("<Return>", do_command)
+
+t.pack()
+c.pack()
+
+
+
+f.pack()
+
+root.mainloop()
+
+#t = '''def handle_special(self, special_key):'''
+#p = re.compile("[a-zA-Z_]+")
+#d = p.search(t)
+#print d
+
+
+
+
 
